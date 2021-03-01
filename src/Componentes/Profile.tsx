82f3599a11cab8422711ea3a34cb2222ee08d983
rodrigocamargo <rodrigocamargo854@gmail.com>
2021-03-01 18:57:47 -0300

    first commit

---
+++ src/Componentes/Profile.tsx
@@ -0,0 +1,17 @@
+
+export function Profile() {
+    return (
+        <div>
+            <div>
+                <img src="https://github.com/rodrigocamargo854" alt="" />
+            </div>
+            <div>
+                <strong> Rodrigo Camargo</strong>
+                <p>Level</p>
+
+            </div>
+        </div>
+
+    );
+}
+
---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import { ExperienceBar } from "../Componentes/ExperienceBar";
+import Head from 'next/head'
+import { Profile } from "../Componentes/Profile";
+
+export default function Home() {
+  return (
+    <div className="container">
+      <ExperienceBar />
+      {/* <Head>
+      <link rel="preconnect" href="https://fonts.gstatic.com"/>
+      <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=Rajdhani:wght@600&display=swap" rel="stylesheet"/> 
+      </Head>  */}
+    </div>
+  )
+}
